3eb86991269017291594b935ba8663d290de426e
Tim Hsieh <tim@fireroaddigital.com>
2024-05-01 17:57:37 -0500

    fix typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
-git config --global aliass.st status
+git config --global alias.st status
 git config --global alias.unstage reset HEAD --
 git config --global alias.last log -1 HEAD
 git config --global alias.graph log --graph --oneline --decorate
