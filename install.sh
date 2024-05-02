8d82c5d962ad16f4e5694cb3b479add68dc085ad
Tim Hsieh <tim@fireroaddigital.com>
2024-05-02 11:03:47 -0500

    add cursor

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ git config --global alias.st status
 git config --global alias.unstage reset HEAD --
 git config --global alias.last log -1 HEAD
 git config --global alias.graph log --graph --oneline --decorate
+
+export PS1='\[^[[01;32m\]\u\[^[[00m\] \[^[[01;34m\]\w\[^[[00m\]$(__git_ps1 " (%s)"):\n'
